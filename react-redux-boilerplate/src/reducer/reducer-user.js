f4129b4b19eb2005515c3423511c122658e8de22
madhusudhanrao beesetty <madhusudhan@madhusudhan-2.local>
2018-08-13 22:51:15 -0700

    completed react-redux-boilerplate

---
+++ react-redux-boilerplate/src/reducer/reducer-user.js
@@ -0,0 +1,14 @@
+export default function () {
+  return [
+        {
+         a:'madhu',
+         b:'this is my first name',
+         c:3
+        },
+        {
+         a:'sudhan',
+         b:'this is my middle name',
+         c:6
+        }
+    ]
+}
